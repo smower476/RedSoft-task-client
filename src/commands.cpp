59bea0a149e978fb616f6b590e4e88e32b6d9e63
Max Pozdnyakov <121474491+smower476@users.noreply.github.com>
2025-07-05 03:12:26 +0300

    commandLoop refactoring

--- src/commands.cpp
+++ src/commands.cpp
@@ -1,12 +1,98 @@
 #include "../include/commands.h"
-#include "../include/connection.h"
 #include "../include/validation.h"
-#include <sys/socket.h>
+#include "../include/connection.h"
 #include <iostream>
-#include <sstream>
 #include <unistd.h>
+#include <sys/socket.h>
+#include <sstream>
 
 using namespace std;
+
+bool handleSend(int sock, const string &channel, const string &nick, istringstream &iss) {
+    string msg;
+    getline(iss, msg);
+    msg = trim(msg);
+    if (msg.empty()) {
+        cout << "Использование: send <сообщение>" << endl;
+        return true;
+    }
+    string request = "send " + channel + " " + nick + " " + msg + "\n";
+    if (send(sock, request.c_str(), request.size(), 0) < 0) {
+        perror("send");
+        return false;
+    }
+    return true;
+}
+
+bool handleRead(int sock, const string &channel, const string &nick) {
+    string request = "read " + channel + " " + nick + "\n";
+    if (send(sock, request.c_str(), request.size(), 0) < 0) {
+        perror("send");
+        return false;
+    }
+
+    string response;
+    if (!recvLine(sock, response)) {
+        cout << "Отключено от сервера." << endl;
+        return false;
+    }
+
+    if (response.rfind("OK", 0) == 0) {
+        istringstream rs(response);
+        string ok;
+        int count;
+        rs >> ok >> count;
+        cout << "Последние " << count << " сообщений в канале '" << channel << "':" << endl;
+        for (int i = 0; i < count; ++i) {
+            if (!recvLine(sock, response)) break;
+            cout << response << endl;
+        }
+    } else {
+        cout << "Ошибка: " << response << endl;
+    }
+
+    return true;
+}
+
+bool handleJoin(int sock, string &channel, const string &nick, istringstream &iss) {
+    string new_channel;
+    iss >> new_channel;
+    if (new_channel.empty()) {
+        cout << "Использование: join <канал>" << endl;
+        return true;
+    }
+
+    if (new_channel.size() > 24) {
+        cout << "Имя канала слишком длинное (максимум 24 символа)" << endl;
+        return true;
+    }
+
+    string request = "join " + new_channel + " " + nick + "\n";
+    if (send(sock, request.c_str(), request.size(), 0) < 0) {
+        perror("send");
+        return false;
+    }
+
+    string response;
+    if (!recvLine(sock, response)) {
+        cout << "Отключено от сервера." << endl;
+        return false;
+    }
+
+    if (response.rfind("OK", 0) == 0) {
+        channel = new_channel;
+        cout << "Вы присоединились к каналу: " << channel << endl;
+    } else {
+        cout << "Ошибка при присоединении: " << response << endl;
+    }
+
+    return true;
+}
+
+bool handleExit(int sock, const string &channel, const string &nick) {
+    string request = "exit " + channel + " " + nick + "\n";
+    return send(sock, request.c_str(), request.size(), 0) >= 0;
+}
 
 void commandLoop(int sock, string &channel, const string &nick) {
     string line;
@@ -21,86 +107,23 @@ void commandLoop(int sock, string &channel, const string &nick) {
         string cmd;
         iss >> cmd;
 
+        bool ok = true;
         if (cmd == "send") {
-            string msg;
-            getline(iss, msg);
-            msg = trim(msg);
-            if (msg.empty()) {
-                cout << "Использование: send <сообщение>" << endl;
-                continue;
-            }
-            string request = "send " + channel + " " + nick + " " + msg + "\n";
-            send(sock, request.c_str(), request.size(), 0);
-        }
-        else if (cmd == "read") {
-            string request = "read " + channel + " " + nick + "\n";
-            send(sock, request.c_str(), request.size(), 0);
-        }
-        else if (cmd == "join") {
-            string new_channel;
-            iss >> new_channel;
-            if (new_channel.empty()) {
-                cout << "Использование: join <канал>" << endl;
-                continue;
-            }
-
-            if (new_channel.size() > 24) {
-                cout << "Имя канала слишком длинное (максимум 24 символа)" << endl;
-                continue;
-            }
-
-            string request = "join " + new_channel + " " + nick + "\n";
-            send(sock, request.c_str(), request.size(), 0);
-
-            string response;
-            if (!recvLine(sock, response)) {
-                cout << "Отключено от сервера." << endl;
-                break;
-            }
-
-            if (response.rfind("OK", 0) == 0) {
-                channel = new_channel;
-                cout << "Вы присоединились к каналу: " << channel << endl;
-            } else {
-                cout << "Ошибка при присоединении: " << response << endl;
-            }
-            continue;
-        }
-        else if (cmd == "exit") {
-            string request = "exit " + channel + " " + nick + "\n";
-            send(sock, request.c_str(), request.size(), 0);
-        }
-        else {
+            ok = handleSend(sock, channel, nick, iss);
+        } else if (cmd == "read") {
+            ok = handleRead(sock, channel, nick);
+        } else if (cmd == "join") {
+            ok = handleJoin(sock, channel, nick, iss);
+        } else if (cmd == "exit") {
+            ok = handleExit(sock, channel, nick);
+        } else {
             cout << "Неизвестная команда. Доступные: send, read, join, exit, quit." << endl;
-            continue;
         }
 
-        string response;
-        if (!recvLine(sock, response)) {
-            cout << "Отключено от сервера." << endl;
+        if (!ok) {
+            cout << "Соединение прервано." << endl;
             break;
-        }
-
-        if (response.rfind("OK", 0) == 0) {
-            if (cmd == "read") {
-                istringstream rs(response);
-                string ok;
-                int count;
-                rs >> ok >> count;
-                cout << "Последние " << count << " сообщений в канале '" << channel << "':" << endl;
-                for (int i = 0; i < count; i++) {
-                    if (!recvLine(sock, response)) break;
-                    cout << response << endl;
-                }
-            } else {
-                cout << "OK" << endl;
-            }
-        }
-        else if (response.rfind("ERROR", 0) == 0) {
-            cout << response << endl;
-        }
-        else {
-            cout << "Неожиданный ответ: " << response << endl;
         }
     }
 }
+
